4f4155b4d5c870cec85fb1354ac8de379b75fbdd
Jason Witkowski <jwitko1@gmail.com>
2021-11-06 01:00:10 -0400

    Minor fixes in prep for v1.0.0 release

--- terraform/rocketpool/bundle.tf
+++ terraform/rocketpool/bundle.tf
@@ -1,8 +1,8 @@
 locals {
   bundle_files = [
-    "${path.root}/../ansible",
-    "${path.root}/../vars",
-    "${path.root}/../requirements.txt"
+    "${path.module}/../../ansible",
+    "${path.module}/../../vars",
+    "${path.module}/../../requirements.txt"
   ]
 }
 
@@ -20,7 +20,7 @@ resource "local_file" "tar_sh" {
 }
 
 data "external" "tar_sh" {
-  program    = [local_file.tar_sh.filename, "-cvzf", "${path.root}/ansible-${local.rp_vars.rocketpool.version}.tar.gz", "${join(" ", local.bundle_files)}"]
+  program    = [local_file.tar_sh.filename, "-cvzf", "${path.module}/ansible-${local.rp_vars.rocketpool.version}.tar.gz", "${join(" ", local.bundle_files)}"]
   depends_on = [data.template_file.tar_sh]
 }
 
@@ -28,8 +28,8 @@ resource "aws_s3_bucket_object" "ansible_tar_gz" {
   depends_on = [data.external.tar_sh]
   bucket     = aws_s3_bucket.deploy.id
   key        = "ansible-${local.rp_vars.rocketpool.version}.tar.gz"
-  source     = "${path.root}/ansible-${local.rp_vars.rocketpool.version}.tar.gz"
-  etag       = try(filemd5("${path.root}/ansible-${local.rp_vars.rocketpool.version}.tar.gz"), null)
+  source     = "${path.module}/ansible-${local.rp_vars.rocketpool.version}.tar.gz"
+  etag       = try(filemd5("${path.module}/ansible-${local.rp_vars.rocketpool.version}.tar.gz"), null)
   tags       = local.default_tags
 
 }
